e4a8f42b14da7bd2f866d4f70ca1687b0448108f
JonasBrunoBenicio <jonas__bruno@hotmail.com>
2025-03-30 19:01:51 +0000

    Cartas cadastradas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,5 +3,26 @@
 int main() {
     printf("Desafio Super Trunfo!\n");
     
+    int População, PontosTurísticos;
+    float Área, PIB;
+    char Estado, Código, Cidade;
+
+   
+    printf("Carta 01: \n");
+    printf("Estado: ");
+    scanf("%s", &Estado);
+    printf("Código: ");
+    scanf("%s", &Código);
+    printf("Cidade: ");
+    scanf("%s", &Cidade);
+    printf("População: ");
+    scanf("%f", &População);
+    printf("Área (em km²): ");
+    scanf("%d", &Área);
+    printf("PIB: ");
+    scanf("%d", &PIB);
+    printf("Número de pontos Turísticos: \n");
+    scanf("%d", &PontosTurísticos);
+
     return 0;
 }
