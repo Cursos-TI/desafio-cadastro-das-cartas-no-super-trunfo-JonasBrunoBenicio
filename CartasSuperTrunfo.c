c9c3b138344b3a6643f20511a0ff8f4164862233
JonasBrunoBenicio <jonas__bruno@hotmail.com>
2025-03-30 19:20:01 +0000

    Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,26 +3,44 @@
 int main() {
     printf("Desafio Super Trunfo!\n");
     
-    int População, PontosTurísticos;
-    float Área, PIB;
-    char Estado, Código, Cidade;
+    int Populacao, PontosTuristicos;
+    float Area, PIB;
+    char Estado, Codigo, Cidade;
 
    
     printf("Carta 01: \n");
     printf("Estado: ");
     scanf("%s", &Estado);
-    printf("Código: ");
-    scanf("%s", &Código);
+    printf("Codigo: ");
+    scanf("%s", &Codigo);
     printf("Cidade: ");
     scanf("%s", &Cidade);
-    printf("População: ");
-    scanf("%f", &População);
-    printf("Área (em km²): ");
-    scanf("%d", &Área);
+    printf("Populacao: ");
+    scanf("%f", &Populacao);
+    printf("Area (em km²): ");
+    scanf("%d", &Area);
     printf("PIB: ");
     scanf("%d", &PIB);
-    printf("Número de pontos Turísticos: \n");
-    scanf("%d", &PontosTurísticos);
+    printf("Número de pontos Turisticos: ");
+    scanf("%d", &PontosTuristicos);
 
+    printf("Carta 02: \n");
+    printf("Estado: ");
+    scanf("%s", &Estado);
+    printf("Codigo: ");
+    scanf("%s", &Codigo);
+    printf("Cidade: ");
+    scanf("%s", &Cidade);
+    printf("Populacao: ");
+    scanf("%f", &Populacao);
+    printf("Area (em km²): ");
+    scanf("%d", &Area);
+    printf("PIB: ");
+    scanf("%d", &PIB);
+    printf("Número de pontos Turisticos: ");
+    scanf("%d", &PontosTuristicos);
+    
     return 0;
+
+
 }
